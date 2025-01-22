7e4053ad79dd840830b5d49d743beec287658a32
Shivraj koli <kolishivraj05@gmail.com>
2025-01-22 22:53:42 +0530

    add approach comment

--- src/App.js
+++ src/App.js
@@ -10,6 +10,20 @@ function App() {
         <Routes>
           <Route path="/" element={<SetupWallet />} />
           <Route path="/transactions" element={<WalletTransactions />} />
+
+          {/*
+            [Approach]:
+
+            We can also implement conditional routing here, wherein if walletId
+            is absent in local storage we can navigate the user to homepage.
+            I find this less user friendly than current implementation which
+            educates the user and ask them to setup their wallet first.
+
+            <Route 
+              path="/transactions" 
+              element={walletId ? <WalletTransactions /> : <Navigate to="/" 
+            />} 
+          /> */}
         </Routes>
       </div>
     </Router>
